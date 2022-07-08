840622d9caebc6efcd665015fc5db5d9734a99c9
Gabriel Akio <akioumebayashi@gmail.com>
2022-07-08 16:47:25 -0300

    Exercicios finalizados

--- src/WebScrapping/SheetsController.php
+++ src/WebScrapping/SheetsController.php
@@ -5,21 +5,25 @@ use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 
 class SheetsController
 {
-
+    /**
+     *  insert the papers data into a sheet
+     * 
+     * @param array $papers
+     */
     public function insertPaper(array $papers): void
     {
         $writer = WriterEntityFactory::createXLSXWriter();
 
-        $writer->openToFile('F:/ws-php/exercicios-php-2022/webscrapping/model.xlsx');
-        $arrayTitle = ['ID','Title','Type'];
+        $writer->openToFile('F:/ws-php/exercicios-php-2022/webscrapping/papers.xlsx'); # defines the path of the sheet
+        $arrayTitle = ['ID','Title','Type']; # create an array with the header
         for ($i = 1; $i <= 10; $i++) {
             array_push($arrayTitle, 'Author ' . $i);
             array_push($arrayTitle, 'Author ' . $i . ' Institution');
         }
-        $rowTitle = WriterEntityFactory::createRowFromArray($arrayTitle);
-        $writer->addRow($rowTitle);
+        $rowTitle = WriterEntityFactory::createRowFromArray($arrayTitle); 
+        $writer->addRow($rowTitle); # insert the array 
         foreach ($papers as $paper) {
-            $arrayAuthor = array_keys($paper->getAuthors());
+            $arrayAuthor = array_keys($paper->getAuthors()); 
             $arrayInstitution = array_values($paper->getAuthors());
             $arrayMerged = array();
             
